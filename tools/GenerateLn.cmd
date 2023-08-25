40232fb265db07c72eebbd06f7dc39628b893d2f
tk <taokeu@gmail.com>
2023-08-25 17:56:26 +0800

    [BLD]

--- tools/GenerateLn.cmd
+++ tools/GenerateLn.cmd
@@ -1,2 +1,2 @@
-dotnet t4 ./GenerateLnResx.tt -o ../assets/resx/Ln.resx
+dotnet t4 ./GenerateLnResx.tt -o ../assets/res/Ln.resx
 dotnet t4 ./GenerateLnCs.tt -o ../dist/Server/NetAdmin.Infrastructure/Ln.cs
