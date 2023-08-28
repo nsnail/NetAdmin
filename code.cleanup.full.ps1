0cf2b15bc9e16fe8e78e61c16c0758915d432c87
tk <taokeu@gmail.com>
2023-08-28 18:24:30 +0800

    [BLD]

---
+++ code.cleanup.full.ps1
@@ -0,0 +1,1 @@
+dotnet jb cleanupcode --no-build ./NetAdmin.sln
---
+++ dot.clean.cmd
@@ -0,0 +1,3 @@
+dot rbom -w -e refs -e .git -e node_modules
+dot trim -w -e refs -e .git -e node_modules
+dot tolf -w -e refs -e .git -e node_modules
