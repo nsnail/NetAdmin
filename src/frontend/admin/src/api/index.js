8d9d031598c2cf7ee22310b09bc8f740cfd1414a
tk <taokeu@gmail.com>
2023-11-17 14:59:17 +0800

    build: 📦 升级到vite5

--- src/frontend/admin/src/api/index.js
+++ src/frontend/admin/src/api/index.js
@@ -2,7 +2,7 @@
  * @description 自动import导入所有 api 模块
  */
 
-const files = import.meta.globEager('./*/*.js')
+const files = import.meta.glob('./*/*.js', { eager: true })
 const modules = {}
 Object.keys(files).forEach((key) => {
     modules[key.replace(/^\.\/(.*?)\/(.*)\.js$/g, '$1_$2')] = files[key].default
--- src/frontend/admin/src/views/home/widgets/components/index.js
+++ src/frontend/admin/src/views/home/widgets/components/index.js
@@ -8,7 +8,7 @@
 import { markRaw } from 'vue'
 
 const resultComps = {}
-const files = import.meta.globEager('./*.vue')
+const files = import.meta.glob('./*.vue', { eager: true })
 Object.keys(files).forEach((fileName) => {
     let comp = files[fileName]
     resultComps[fileName.replace(/^\.\/(.*)\.\w+$/, '$1')] = comp.default
