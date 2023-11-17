3bb51876e7f0ef6766f06cb79a7d699e9f91859c
tk <taokeu@gmail.com>
2023-11-17 14:37:40 +0800

    wip: 🧠 1

--- 1.git.pr.ps1
+++ 1.git.pr.ps1
@@ -5,7 +5,7 @@ git cz
 git pull
 git push --set-upstream origin $branch
 Start-Process -FilePath "https://github.com/nsnail/NetAdmin/compare/main...$branch"
-Write-Host "按任意键重建分支，Ctrl+C 退出"
+Write-Host "按 「 Enter 」 重建分支，「 Ctrl+C 」 退出"
 Pause
 git checkout main
 git pull
