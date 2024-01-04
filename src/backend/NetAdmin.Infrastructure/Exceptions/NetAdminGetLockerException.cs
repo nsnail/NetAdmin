18678fb8a002f6ab92b214b72cf7016de07e285d
tk <taokeu@gmail.com>
2024-01-04 09:29:40 +0800

    perf: ⚡ 依赖组件升级

--- src/backend/NetAdmin.Infrastructure/Exceptions/NetAdminGetLockerException.cs
+++ src/backend/NetAdmin.Infrastructure/Exceptions/NetAdminGetLockerException.cs
@@ -4,6 +4,5 @@ namespace NetAdmin.Infrastructure.Exceptions;
 ///     加锁失败异常
 /// </summary>
 #pragma warning disable RCS1194
-public sealed class NetAdminGetLockerException : NetAdminUnexpectedException
+public sealed class NetAdminGetLockerException : NetAdminUnexpectedException;
 #pragma warning restore RCS1194
-{ }
