7501138e313d4c913ed691e45b95e0f8cb26931a
nsnail <taokeu@gmail.com>
2024-11-27 17:58:38 +0800

    chore: 🔨 界面优化 (#226)
    Co-authored-by: tk <fiyne1a@dingtalk.com>

--- src/frontend/admin/src/config/naColOperation.js
+++ src/frontend/admin/src/config/naColOperation.js
@@ -2,12 +2,14 @@ export default {
     buttons: [
         {
             icon: 'el-icon-view',
+            title: '查看',
             click: async (row, vue) => {
                 vue.dialog.save = { row, mode: 'view' }
             },
         },
         {
             icon: 'el-icon-edit',
+            title: '编辑',
             click: async (row, vue) => {
                 vue.dialog.save = { row, mode: 'edit' }
             },
