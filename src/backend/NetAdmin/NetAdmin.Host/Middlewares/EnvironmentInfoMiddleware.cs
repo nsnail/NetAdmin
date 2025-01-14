5eb0b6da00a03f8dc4fb4dcb9d558db0d4642a06
tk <fiyne1a@dingtalk.com>
2025-01-14 15:31:16 +0800

    chore: 🔨 npm update

---
+++ src/backend/NetAdmin/NetAdmin.Host/Middlewares/EnvironmentInfoMiddleware.cs
@@ -0,0 +1,16 @@
+﻿namespace NetAdmin.Host.Middlewares;
+
+/// <summary>
+///     输出环境信息到 http header
+/// </summary>
+public sealed class EnvironmentInfoMiddleware(RequestDelegate next)
+{
+    /// <summary>
+    ///     主函数
+    /// </summary>
+    public Task InvokeAsync(HttpContext context)
+    {
+        context.Response.Headers.Append("X-Node", Environment.MachineName);
+        return next(context);
+    }
+}
